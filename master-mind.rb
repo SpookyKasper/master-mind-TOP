e17aeddcd236924c8def9d7f0c8b9d055cd8b4ac
Kasper <kasperbak89@pm.me>
2022-09-13 00:36:07 +0200

    Make it more OOP

--- master-mind.rb
+++ master-mind.rb
@@ -1,83 +1,83 @@
-=begin
-begin pseudo code:
-
-Build the game assuming the computer randomly selects the secret colors
-and the human player must guess them. Remember that you need to give the
-proper feedback on how good the guess was each turn!
-
-
-make the computer generate a random code of 4 letters (first letter of a color)
-from a collection of 6 letters
-
-tell the player what are is options and ask the player to input is guess,
-compare it to the computer code and give a feedback to the user
-on how accurate is guess was
-
-display:
-
-make 12 rows of 2 arrays of 4 slots each, one is for the player to
-guess, the other one is for the feedback of the computer.
-the array of the feedback shows 0 if the color is not present in the
-code, 1 if the color is present but it the wrong spot and 2 if
-the color is present and in the right spot
-
-Yellow
-Orange
-Purple
-Blue
-White
-Cyan
-=end
-
 # display some explanation for the player
-puts "Hello player! what is your buest guess for this mastermind game?"
+puts "Hello player!"
 puts "Please make your guess using a combination of 4 colors seperated with spaces"
 puts "Example: Green yellow blue white"
 puts "Or: cyan white blue orange"
+puts "Feedback will be given by an array of numbers"
+puts "A 2 means some color of the guess is present in the code and in the same place as in the code"
+puts "A 1 means some color is present in the code but not in the same place as in the code"
+puts "A 0 means the color is not present in the code"
+puts "Example: imagine the code is 'yellow orange blue white'"
+puts "And you made a guess of 'orange green blue yellow'"
+puts "You'll get a feedback similar to [2, 1, 1, 0]"
+puts "Indeed one color (the blue) is present and in the same place"
+puts "Two colors (the yellow and orange) are present but not in the same place"
+puts "And finally one of the colors (the green) is not present in the code"
+puts "Got it?"
+puts "Coool! Let's go!"
 puts "You have 12 tries, good luck!"
-puts "Let's go!"
 
-# declare an array with the colors available
-colors = %w(yellow orange purple blue white cyan)
-
-# make the computer generate a random code based on the six possible colors
-code_to_find = colors.sample(4)
-
-# # declare a board made of arrays with nil elements and print it
-# board = Array.new(12) {Array.new(4)}
-# board.each do |v|
-#   p v
-# end
-
-# make a loop that keeps asking for user guesses until they got the code or until
-# they run out of guesses
-time_to_stop = false
-player_found_code = false
-guesses_left = 12
-until (time_to_stop)
-  if guesses_left < 12
-    puts "Not yet! try again! you have #{guesses_left} guesses left!"
+class Mastermind
+  def initialize
+    @player_found_code = false
+    @guesses_left = 12
+    @colors = %w(yellow orange purple blue white cyan)
   end
-  puts "Remember, the possible colors are: #{colors}"
-  puts "So darling what is your guess ?"
-  guess = gets.chomp.downcase.split
-  # compare guess and code to find
-  feedback_array = []
-  guess.each_with_index do |color, index|
-    if code_to_find.index(color) == index then feedback_array << 2
-    elsif code_to_find.include?(color) && code_to_find.index(color) != index then feedback_array << 1
-    else feedback_array << 0
+
+  def get_user_input
+    puts "You have #{@guesses_left} guesses left"
+    puts "Remember, the possible colors are: #{@colors}"
+    puts "So darling what is your guess ?"
+    gets.chomp.downcase.split
+  end
+
+  def gen_computer_code
+    @colors.sample(4)
+  end
+
+  def gen_user_code
+    puts "Pick four colors for your secret code"
+    puts "Remember the colors are #{@colors}"
+    gets.chomp.downcase.split
+  end
+
+  def compare_guess_with_code(guess, code)
+    feedback_array = []
+    guess.each_with_index do |color, index|
+      if code.index(color) == index then feedback_array << 2
+      elsif code.include?(color) && code.index(color) != index then feedback_array << 1
+      else feedback_array << 0
+      end
+    end
+    feedback_array
+  end
+
+  def code_cracked?(feedback)
+    feedback.count(2) == 4
+  end
+
+  def player_won
+    puts "Congratulations"
+  end
+
+  def gameover?
+    @guesses_left == 0 || @player_found_code
+  end
+
+  def play
+    code = gen_computer_code
+    until gameover?
+      guess = get_user_input
+      feedback = compare_guess_with_code(guess, code)
+      p code
+      p "Your guess was '#{guess.join(" ")}'"
+      p "And the feedback for your guess is #{feedback.sort.reverse}"
+      @guesses_left -= 1
+      if code_cracked?(feedback)
+        @player_found_code = true end
     end
   end
-  p guess
-  p code_to_find
-  p feedback_array.shuffle
-  if feedback_array.join == '2222'
-    puts "Congratulations, you cracked the code!"
-    time_to_stop = true
-  end
-  guesses_left -= 1
-  if guesses_left == 0 then time_to_stop = true end
 end
 
-
+mastermind = Mastermind.new
+mastermind.play
