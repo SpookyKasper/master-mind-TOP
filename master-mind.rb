f02a60899ed79197c8024578e44e7e7b80f1b401
Kasper <kasperbak89@pm.me>
2022-09-13 21:09:53 +0200

    Make the code display previous guesses with feedback

--- master-mind.rb
+++ master-mind.rb
@@ -1,15 +1,17 @@
 # display some explanation for the player
 puts "Hello player!"
 puts "Please make your guess using a combination of 4 colors seperated with spaces"
-puts "Example: Green yellow blue white"
-puts "Or: cyan white blue orange"
+puts "Example: orange yellow blue white"
 puts "Feedback will be given by an array of numbers"
-puts "A 2 means some color of the guess is present in the code and in the same place as in the code"
-puts "A 1 means some color is present in the code but not in the same place as in the code"
-puts "A 0 means the color is not present in the code"
-puts "Example: imagine the code is 'yellow orange blue white'"
-puts "And you made a guess of 'orange green blue yellow'"
-puts "You'll get a feedback similar to [2, 1, 1, 0]"
+puts "A 2 means some color of the guess is present in the secret code and in the same place"
+puts "A 1 means some color is present but not in the same place"
+puts "A 0 means the color is not present in the secret code "
+puts "Example: imagine the code is
+'yellow orange blue white'"
+puts "And you made a guess of
+'orange green blue yellow'"
+puts "You'll get a feedback of
+[2, 1, 1, 0]"
 puts "Indeed one color (the blue) is present and in the same place"
 puts "Two colors (the yellow and orange) are present but not in the same place"
 puts "And finally one of the colors (the green) is not present in the code"
@@ -17,41 +19,15 @@ puts "Got it?"
 puts "Coool! Let's go!"
 puts "You have 12 tries, good luck!"
 
-# class Board
-#   def initialize(rows, columns)
-#     @rows = rows
-#     @columns = columns
-#     @board = Array.new(rows) {Array.new(columns)}
-#   end
-
-#   def display_board
-#     @board.each {|row| p row}
-#   end
-# end
-
-# my_master_board = Board.new(12, 4)
-# my_master_board.display_board
-
 class Mastermind
   def initialize
-    @player_found_code = false
+    @code_cracked = false
     @guesses_left = 12
     @colors = %w(yellow orange purple blue white cyan)
     @computer_code = @colors.sample(4)
-  end
-
-  def get_user_input
-    puts "You have #{@guesses_left} guesses left"
-    puts "Remember, the possible colors are: #{@colors}"
-    puts "So darling what is your guess ?"
-    gets.chomp.downcase.split
-  end
-
-  def input_valid?(input)
-    bolleaned_input = input.map do |color|
-      p @colors.include?(color)
-    end
-    !bolleaned_input.include?(false) && bolleaned_input.length == 4
+    @user_code = nil
+    @guesses_array = []
+    @feedbacks_array = []
   end
 
   def gen_user_code
@@ -60,40 +36,62 @@ class Mastermind
     gets.chomp.downcase.split
   end
 
-  def compare_guess_with_code(guess, code)
-    feedback_array = []
-    guess.each_with_index do |color, index|
-      if code.index(color) == index then feedback_array << 2
-      elsif code.include?(color) && code.index(color) != index then feedback_array << 1
-      else feedback_array << 0
+  def get_user_input
+    valid = false
+    until valid
+      puts "You have #{@guesses_left} guesses left"
+      puts "Remember, the possible colors are: #{@colors}"
+      puts "So darling what is your guess ?"
+      guess = gets.chomp.downcase.split
+      if guess.all? { |color| @colors.include?(color) } && guess.length == 4
+        @guesses_array << guess
+        valid = true
+      else
+        puts 'Mmh something is wrong with your input, please type 4 colors seperated with spaces'
+        puts 'Like so: yellow orange blue green'
       end
     end
-    feedback_array
+    guess
+  end
+
+  def compare_guess_with_code(guess, code)
+    feedback = []
+    guess.each_with_index do |color, index|
+      if code.index(color) == index then feedback << 2
+      elsif code.include?(color) && code.index(color) != index then feedback << 1
+      else feedback << 0
+      end
+    end
+    @feedbacks_array << feedback.sort.reverse
+    feedback
   end
 
   def code_cracked?(feedback)
-    feedback.count(2) == 4
+    feedback.all?(2)
   end
 
   def gameover?
-    @guesses_left == 0 || @player_found_code
+    @guesses_left == 0 || @code_cracked
+  end
+
+  def display_sofar
+    round = 0
+    while round < @guesses_array.length
+    puts "Your pair guess & feedback for guess #{round + 1} was " +
+         "#{@guesses_array[round]}" + " #{@feedbacks_array[round]}"
+    round += 1
+    end
   end
 
   def play_against_computer
     until gameover?
       guess = get_user_input
-      unless input_valid?(guess)
-        puts 'Mmh something is wrong with your input, please type 4 colors seperated with spaces'
-        puts 'Like so: yellow orange blue green'
-        next
-      end
       feedback = compare_guess_with_code(guess, @computer_code)
-      p code
-      puts "Your guess was '#{guess.join(" ")}'"
-      puts "And the feedback for your guess is #{feedback.sort.reverse}"
       @guesses_left -= 1
+      display_sofar
       if code_cracked?(feedback)
-        @player_found_code = true
+        puts "Congratulations!! you cracked the code!!!"
+        @code_cracked = true
       end
     end
   end
